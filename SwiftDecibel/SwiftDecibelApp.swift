13bf79562cbe17e8fb3170cb8b984490dea838d7
mafyyu <morita0126m@gmail.com>
2025-03-17 11:20:37 +0900

    Initial Commit

---
+++ SwiftDecibel/SwiftDecibelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDecibelApp.swift
+//  SwiftDecibel
+//
+//  Created by 森田将嵩 on 2025/03/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDecibelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
